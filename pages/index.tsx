0963515f8421b62c4dffdb8d387d682cc164dfe7
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2023-11-10 20:45:58 +0200

    first additions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+import data from "../data.json";
 
 export default function Home() {
   return (
-    
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
+      <Image 
+      className="rounded-full"
+        alt={data.name}
+        src={data.avatar}
+        width={96}
+        height={96}
+      />
+      <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
+    </div>
   )
 }
