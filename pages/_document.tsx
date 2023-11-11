f8804e5f161091bedaf4ea48e0b3caadfcad4a30
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2023-11-11 21:52:13 +0200

    minor additions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>SAMURAI</title>
+      <link rel="icon" type="image/png" href="https://cdn2.steamgriddb.com/file/sgdb-cdn/icon/b3e3e393c77e35a4a3f3cbd1e429b5dc/32/64x64.png" />
       <body className=''>
         <Main />
         <NextScript />
